50af0f8c64274c36d71c827d9de9e229ab2af0d4
LayerDE <the_gamer@gmx.net>
2023-04-26 14:06:24 +0200

    implement oid follower

--- main/pushed_follower.cpp
+++ main/pushed_follower.cpp
@@ -3,6 +3,18 @@
 #include "defines.h"
 #include "config.h"
 #include "math_functions.h"
+#include "pushed_follower.hpp"
+
+follower::follower(int to_car_axle, int to_follower_axle){
+    alpha_calc = PID(&isPoint, &output, &setPoint,0.0,0.0,0.0,0);
+}
+
+double follower::calculate(int des_speed, float des_steering){
+    isPoint = get_steering();
+    setPoint = des_steering;
+    alpha_calc.Compute();
+    return output;
+}
 
 float calc_beta_const(float alpha_steer){
     if(alpha_steer == 0.0)
@@ -10,7 +22,7 @@ float calc_beta_const(float alpha_steer){
     float V_bw = L_WHEELBASE/tan(fabs(alpha_steer));
     return tan(L_REAR_TO_HITCH/V_bw);
     float delta_2 = sin(L_HITCH_TO_FOLLOWER_AXLE/sqrt(pow2(V_bw)+pow2(L_REAR_TO_HITCH)));
-    return delta_1;
+    //return delta_1;
 }
 
 float calc_alpha_const(float beta){
@@ -20,10 +32,23 @@ float calc_alpha_const(float beta){
 
 float calc_distance(float alpha, float beta_old, float beta_new){
     if(beta_old == beta_new)
+    {
         return 0.0;
-    else
-        return 0;
+    }
+    else if(calc_beta_const(alpha)==beta_new)
+    {
+        return FLOAT_MAX;
+    }
+        
 }
 float calc_alpha(float distance, float beta_old, float beta_new){
+    if(beta_old == beta_new)
+    {
+        return calc_alpha_const(beta_new);
+    }
+    else if(beta_old>)
+    {
+
+    }
 
 }
--- main/pushed_follower.hpp
+++ main/pushed_follower.hpp
@@ -1,12 +1,17 @@
 #pragma once
+#include "PID_v1.h"
 
 class follower{
     public:
         follower(int to_car_axle, int to_follower_axle);
         int get_speed();
         float get_steering();
-        void calculate(int des_speed, float des_steering);
+        double calculate(int des_speed, float des_steering);
     private:
+        PID alpha_calc;
+        double setPoint;
+        double isPoint;
+        double output;
         void math_model();
         int lenght;
         int axle;
