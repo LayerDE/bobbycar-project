d67e4efa25d00e6b4a3604e7d16f092c10a5d7c3
LayerDE <the_gamer@gmx.net>
2023-04-28 01:00:53 +0200

    stackable follower interface

--- main/pushed_follower.cpp
+++ main/pushed_follower.cpp
@@ -1,35 +1,38 @@
 #include <math.h>
 #include <stdint.h>
 #include "defines.h"
-#include "config.h"
 #include "math_functions.h"
 #include "pushed_follower.hpp"
 
 follower::follower(int to_car_axle, int to_follower_axle){
-    alpha_calc = PID(&isPoint, &output, &setPoint,0.0,0.0,0.0,0);
+    alpha_calc = new PID(&isPoint, &output, &setPoint, 0.0, 0.0, 0.0, 0);
+}
+
+follower::~follower(){
+    delete alpha_calc;
 }
 
 double follower::calculate(int des_speed, float des_steering){
     isPoint = get_steering();
     setPoint = des_steering;
-    alpha_calc.Compute();
+    alpha_calc->Compute();
     return output;
 }
 
-float calc_beta_const(float alpha_steer){
+float follower::calc_beta_const(float alpha_steer){
     if(alpha_steer == 0.0)
         return 0.0;
-    float V_bw = L_WHEELBASE/tan(fabs(alpha_steer));
-    return tan(L_REAR_TO_HITCH/V_bw);
-    float delta_2 = sin(L_HITCH_TO_FOLLOWER_AXLE/sqrt(pow2(V_bw)+pow2(L_REAR_TO_HITCH)));
+    float V_bw = car_wheelbase/tan(fabs(alpha_steer));
+    return tan(car2hitch/V_bw);
+    float delta_2 = sin(hitch2axle/sqrt(pow2(V_bw)+pow2(car2hitch)));
     //return delta_1;
 }
 
-float calc_alpha_const(float beta){
-    float V_bw = L_HITCH_TO_FOLLOWER_AXLE/tan(beta);
-    return atan(L_WHEELBASE/V_bw);
+float follower::calc_alpha_const(float beta){
+    float V_bw = hitch2axle/tan(beta);
+    return atan(car_wheelbase/V_bw);
 }
-
+/*
 float calc_distance(float alpha, float beta_old, float beta_new){
     if(beta_old == beta_new)
     {
@@ -37,7 +40,7 @@ float calc_distance(float alpha, float beta_old, float beta_new){
     }
     else if(calc_beta_const(alpha)==beta_new)
     {
-        return FLOAT_MAX;
+        return INFINITY;
     }
         
 }
@@ -52,3 +55,4 @@ float calc_alpha(float distance, float beta_old, float beta_new){
     }
 
 }
+*/
--- main/pushed_follower.hpp
+++ main/pushed_follower.hpp
@@ -1,19 +1,28 @@
 #pragma once
 #include "PID_v1.h"
 
+
+typedef float (*get_float)();
+typedef int (*get_int)();
+
+
 class follower{
     public:
-        follower(int to_car_axle, int to_follower_axle);
-        int get_speed();
-        float get_steering();
+        follower(int rc_axle2hitch, int hitch2car_axle);
+        ~follower();
         double calculate(int des_speed, float des_steering);
     private:
-        PID alpha_calc;
+        PID *alpha_calc;
+        float calc_beta_const(float alpha_steer);
+        float calc_alpha_const(float beta);
         double setPoint;
         double isPoint;
         double output;
-        void math_model();
-        int lenght;
-        int axle;
+        get_float get_steering;
+        get_float get_hitch_angle;
+        get_int get_speed;
+        int hitch2axle;
+        int car2hitch;
+        int car_wheelbase;
 
 };
