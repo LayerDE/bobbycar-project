b4aa4645239ef3b7fbd37c728fdabf70c3c9a8f4
LayerDE <the_gamer@gmx.net>
2023-08-17 00:40:57 +0200

    fix clamp for low>high

--- main/defines.h
+++ main/defines.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#define _CLAMP(x, low, high) (((x) > (high)) ? (high) : (((x) < (low)) ? (low) : (x)))
+
+
 #define ABS(a) (((a) < 0.0) ? -(a) : (a))
 #define LIMIT(x, lowhigh) (((x) > (lowhigh)) ? (lowhigh) : (((x) < (-lowhigh)) ? (-lowhigh) : (x)))
 #define SAT(x, lowhigh) (((x) > (lowhigh)) ? (1.0) : (((x) < (-lowhigh)) ? (-1.0) : (0.0)))
@@ -10,7 +13,7 @@
 #define SIGN(a) (((a) < 0.0) ? (-1.0) : (((a) > 0.0) ? (1.0) : (0.0)))
 #define SIGN_INT(a) (((a) < 0) ? (-1) : (((a) > 0) ? (1) : (0)))
 //#define sign SIGN
-#define CLAMP(x, low, high) (((x) > (high)) ? (high) : (((x) < (low)) ? (low) : (x)))
+#define CLAMP(x, low, high) (low > high ? _CLAMP(x, high, low): _CLAMP(x,low,high))
 #define SCALE(value, high, max) MIN(MAX(((max) - (value)) / ((max) - (high)), 0.0), 1.0)
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
