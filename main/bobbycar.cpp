23031958e9fcfdc8d5044871b1bd69cac85a9275
LayerDE <the_gamer@gmx.net>
2023-10-02 02:45:05 +0200

    recive testings

--- main/bobbycar.cpp
+++ main/bobbycar.cpp
@@ -21,31 +21,36 @@ void Send(EspSoftwareSerial::UART* board, int16_t speed0, int16_t speed1) {
 }
 
 // ########################## RECEIVE ##########################
+static inline uint8_t new_idx(uint8_t old_idx, uint8_t summand){
+    return (old_idx + summand) % sizeof(SerialFeedback);
+}
 
 bool Receive(EspSoftwareSerial::UART* board, SerialFeedback* out, SerialVariables *vars, unsigned long time) {
-    uint16_t bufStartFrame;  // Buffer Start Frame
-    // byte buffer[sizeof(SerialFeedback)];
-    //  Check for new data availability in the Serial buffer
+    uint16_t bufStartFrame; // Buffer Start Frame
+    // Check for new data availability in the Serial buffer
     bool data_complete = false;
     while(board->available()){
         vars->lastUpdate = time;
-        vars->buffer[vars->idx = (vars->idx + 1) % sizeof(SerialFeedback)] = board->read();                                       // Read the incoming byte
-        bufStartFrame = ((uint16_t)(vars->buffer[(vars->idx + 2) % sizeof(SerialFeedback)]) << 8)
-            | (uint16_t)vars->buffer[(vars->idx + 1) % sizeof(SerialFeedback)];  // Construct the start frame
+        vars->buffer[vars->idx = new_idx(vars->idx,1)] = board->read(); // Read the incoming byte
+        bufStartFrame = ((uint16_t)(vars->buffer[new_idx(vars->idx, 2)]) << 8)
+            | (uint16_t)vars->buffer[new_idx(vars->idx, 1)];  // Construct the start frame
 
             // Copy received data
         if (bufStartFrame == START_FRAME) {  // Initialize if new data is detected
         //printf("start frame...");
             SerialFeedback NewFeedback;
-            int pack_start = (vars->idx + 1) % sizeof(SerialFeedback);
+            int pack_start = new_idx(vars->idx, 1);
+
             memcpy(&NewFeedback, &(vars->buffer[pack_start]),sizeof(SerialFeedback)-pack_start);
             memcpy(&(((uint8_t*)&NewFeedback)[sizeof(SerialFeedback)-pack_start]), vars->buffer,pack_start);
+            //for(int x = 0; x < sizeof(SerialFeedback); x++)
+            //    ((uint8_t*)&NewFeedback)[x] = vars->buffer[new_idx(pack_start,x)];
             uint32_t checksum = calc_crc32((uint8_t*)&NewFeedback, sizeof(SerialFeedback) - sizeof(uint16_t)*2);
             uint32_t checksum_package = (uint32_t)NewFeedback.checksumL | ((uint32_t)NewFeedback.checksumH << 16);
-            // printf("%x == %x\n",checksum_package,checksum);
+            // printf("%x == %x\n",(unsigned int)checksum_package,(unsigned int)checksum);
             // Check validity of the new data
             if (checksum == checksum_package) {
-                //printf("crc correct\n");
+                // printf("crc correct\n");
                 // Copy the new data
                 memcpy(out, &NewFeedback, sizeof(SerialFeedback));
                 // Print data to built-in Serial
